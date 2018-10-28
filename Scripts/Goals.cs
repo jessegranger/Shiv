07c42041ee84961a884db33530dc31197a493e71
Jesse Granger <jesse_granger@fastmail.com>
2018-10-27 20:12:30 -0700

    Add Goals and Senses (with Scripts to tick them).

---
+++ Scripts/Goals.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static Shiv.Globals;
+
+namespace Shiv {
+	public enum GoalStatus {
+		Active,
+		Paused,
+		Complete,
+		Failed
+	}
+	public abstract class Goal {
+		public GoalStatus Status = GoalStatus.Paused;
+		public abstract GoalStatus OnTick();
+		public virtual void OnPause() { Status = GoalStatus.Paused; }
+		public virtual void OnResume() { Status = GoalStatus.Active; }
+	}
+
+	public class GoalSet {
+		private LinkedList<Goal> goals = new LinkedList<Goal>();
+		public void Push(Goal g) => goals.AddFirst(g);
+		public Goal Pop() => goals.Pop();
+		public Goal Peek() => goals.First?.Value;
+		public void Clear() => goals.Clear();
+		public int Count => goals.Count;
+		public override string ToString() => $"Goals({Count}): " + string.Join(", ", goals.Select(g => g.ToString()));
+	}
+
+	public static partial class Globals {
+		public static GoalSet Goals = new GoalSet();
+	}
+
+	public class GoalScript : Script {
+		private Goal prevFirst = null;
+		private static bool IsDone(GoalStatus? status) => status.HasValue && (status.Value == GoalStatus.Failed || status.Value == GoalStatus.Complete);
+		public override void OnTick() {
+			while( Goals.Count > 0 && IsDone(Goals.Peek().Status) ) {
+				Goals.Pop();
+				prevFirst = null;
+			}
+			if( Goals.Count > 0 ) {
+				Goal curFirst = Goals.Peek();
+				if( prevFirst != curFirst ) { // pause and resume pre-empted tasks as the top item changes
+					if( prevFirst?.Status == GoalStatus.Active ) prevFirst.OnPause();
+					if( curFirst.Status == GoalStatus.Paused ) curFirst.OnResume();
+					prevFirst = curFirst;
+				}
+				curFirst.OnTick();
+			}
+			UI.DrawText(Goals.ToString());
+		}
+	}
+}
---
+++ Scripts/Senses.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using static GTA.Native.Function;
+using static GTA.Native.Hash;
+using static Shiv.Globals;
+
+namespace Shiv {
+	public static partial class Globals {
+		public static SenseSet Senses = new SenseSet();
+	}
+
+	public class SenseSet {
+		private List<Sense> senses = new List<Sense>();
+		public void Add(Sense s) => senses.Add(s);
+		public void OnTick() => senses.Each(s => s.OnTick());
+	}
+	public abstract class Sense {
+		public abstract void OnTick();
+	}
+
+	public class SenseScript : Script {
+		public override void OnInit() {
+			Senses.Add(new DangerSense());
+		}
+		public override void OnTick() {
+			Senses.OnTick();
+		}
+	}
+
+	public class DangerSense : Sense {
+		public override void OnTick() {
+			foreach( PedHandle ped in NearbyHumans ) {
+				BlipHandle blip = Call<BlipHandle>(GET_BLIP_FROM_ENTITY, ped);
+				if( blip != BlipHandle.Invalid ) {
+					if( GetColor(blip) == BlipColor.Red ) {
+						DrawSphere(Position(ped) + Up, .1f, Color.Red);
+					}
+				}
+			}
+		}
+	}
+}
