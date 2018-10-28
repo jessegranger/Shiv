31fd36bb62d030f72a44e403ba057924e52aa86b
Jesse Granger <jesse_granger@fastmail.com>
2018-10-27 20:10:44 -0700

    Add DrawLine() and DrawSphere()

--- Game/World.cs
+++ Game/World.cs
@@ -3,37 +3,49 @@ using System;
 using System.Drawing;
 using GTA.Native;
 using System.Numerics;
+using static GTA.Native.Function;
+using static GTA.Native.Hash;
 
 namespace Shiv {
 	public static partial class Globals {
 
 		public static PointF ScreenCoords(Vector3 pos) {
 			float x, y;
-			unsafe { Function.Call<bool>(Hash._GET_SCREEN_COORD_FROM_WORLD_COORD, pos, new IntPtr(&x), new IntPtr(&y)); }
+			unsafe {
+				Call<bool>(_GET_SCREEN_COORD_FROM_WORLD_COORD,
+					pos, new IntPtr(&x), new IntPtr(&y)); }
 			return new PointF(x, y);
-		}
+		} // TODO: measure the speed of this versus using CameraMatrix directly
 
 		public static Vector3 PutOnGround(Vector3 pos, float off = 0) => new Vector3(pos.X, pos.Y, off + GetGroundZ(pos));
 		public static float GetGroundZ(Vector3 pos) {
 			float z = 0;
 			unsafe {
-				Function.Call(Hash.GET_GROUND_Z_FOR_3D_COORD,
+				Call(GET_GROUND_Z_FOR_3D_COORD,
 					pos.X, pos.Y, pos.Z + .05f, 
 					new IntPtr(&z), 0);
 			}
 			return z;
 		}
 
-		public static void PauseClock(bool value) => Function.Call(Hash.PAUSE_CLOCK, value);
+		public static void DrawLine(Vector3 start, Vector3 end, Color color) => Call(DRAW_LINE, start, end, color);
+		public static void DrawSphere(Vector3 pos, float radius, Color color) {
+			Call(DRAW_MARKER, MarkerType.DebugSphere, 
+				pos, Vector3.Zero, Vector3.Zero,
+				new Vector3(radius, radius, radius),
+				color, false, false, 2, 0, 0, 0, 0);
+		}
 
-		public static void Blackout(bool value) => Function.Call(Hash._SET_BLACKOUT, value);
+		public static void PauseClock(bool value) => Call(PAUSE_CLOCK, value);
 
-		public static void ChangeWeather(Weather weather, float duration) => Function.Call(Hash._SET_WEATHER_TYPE_OVER_TIME, Enum.GetName(typeof(Weather), weather), duration);
+		public static void Blackout(bool value) => Call(_SET_BLACKOUT, value);
+
+		public static void ChangeWeather(Weather weather, float duration) => Call(_SET_WEATHER_TYPE_OVER_TIME, Enum.GetName(typeof(Weather), weather), duration);
 
 		public static float Gravity() => MemoryAccess.ReadWorldGravity();
 		public static void Gravity(float value) {
 			MemoryAccess.WriteWorldGravity(value);
-			Function.Call(Hash.SET_GRAVITY_LEVEL, 0);
+			Call(SET_GRAVITY_LEVEL, 0);
 			MemoryAccess.WriteWorldGravity(9.800000f);
 		}
 
@@ -49,7 +61,7 @@ namespace Shiv {
 		    NativeVector3 normal;
 				int material;
 		    int entity;
-				unsafe { Function.Call(Hash._GET_SHAPE_TEST_RESULT_EX, result, 
+				unsafe { Call(_GET_SHAPE_TEST_RESULT_EX, result, 
 					new IntPtr(&hit), 
 					new IntPtr(&hitPos), 
 					new IntPtr(&normal), 
@@ -67,9 +79,9 @@ namespace Shiv {
 		public static RaycastResult Raycast(Vector3 source, Vector3 target, float radius, IntersectOptions options, PedHandle ignoreEntity) =>
 			Raycast(source, target, radius, options, (int)ignoreEntity);
 		public static RaycastResult Raycast(Vector3 source, Vector3 target, IntersectOptions options, int ignoreEntity) => 
-			new RaycastResult(Function.Call<int>(Hash._START_SHAPE_TEST_RAY, source, target, options, ignoreEntity, 7));
+			new RaycastResult(Call<int>(_START_SHAPE_TEST_RAY, source, target, options, ignoreEntity, 7));
 		public static RaycastResult Raycast(Vector3 source, Vector3 target, float radius, IntersectOptions options, int ignoreEntity) =>
-			new RaycastResult(Function.Call<int>(Hash.START_SHAPE_TEST_CAPSULE, source, target, radius, options, ignoreEntity, 7));
+			new RaycastResult(Call<int>(START_SHAPE_TEST_CAPSULE, source, target, radius, options, ignoreEntity, 7));
 		public static bool IsCarMaterial(Materials m) =>
 			m == Materials.car_metal ||
 			m == Materials.car_engine ||
