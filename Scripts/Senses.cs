9fff3b5cc4c2480ee3e49249a7ac94cca15781fb
Jesse Granger <jesse@conductrics.com>
2019-07-09 21:25:14 -0700

    add Waypoint blip to senses

--- Scripts/Senses.cs
+++ Scripts/Senses.cs
@@ -61,22 +61,6 @@ namespace Shiv {
 		}
 	}
 
-	public class CoverSense : Sense {
-		private static readonly NodeHandle[] Empty = new NodeHandle[0];
-		public static NodeHandle[] NearbyCover = Empty;
-		private bool IsStopped(Task t) => (t.IsCanceled || t.IsFaulted || t.IsCompleted);
-		public CoverSense() => Log("CoverSense: Starting...");
-		public override void OnTick() {
-			UI.DrawText($"Cover: {NearbyCover.Length}");
-			int i = 0;
-			foreach( NodeHandle n in NearbyCover.Take(20) ) {
-				DrawSphere(Position(n), .1f, Color.Blue);
-				UI.DrawTextInWorld(Position(n) + (Up * i/55), $"{i}:{n}");
-				i++;
-			}
-		}
-	}
-
 	public class BlipSense : Sense {
 		public static bool ShowBlips = true;
 		public override void OnTick() {
@@ -89,13 +73,10 @@ namespace Shiv {
 					BlipHUDColor blipColor = GetBlipHUDColor(blip);
 					msg += $"{blipColor} ";
 					Color color = GetColor(blipColor);
-					DrawSphere(pos + Up, .3f, color);
+					DrawSphere(pos + Up, .2f, color);
 				}
 			}
-			foreach( var blip in GetAllBlips(BlipSprite.Safehouse) ) {
-				UI.DrawText($"Safehouse: {DistanceToSelf(Position(blip))}");
-			}
-			Items(BlipSprite.Standard, BlipSprite.PoliceOfficer).Each(s =>
+			Items(BlipSprite.Standard, BlipSprite.Waypoint).Each(s =>
 				UI.DrawText($"Blips({s}): {string.Join(" ", GetAllBlips(s).Select(GetBlipColor))}")
 			);
 		}
