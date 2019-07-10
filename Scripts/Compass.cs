8c318269af85e4af781f8f23b5510101c0871dea
Jesse Granger <jesse@conductrics.com>
2019-07-09 21:26:08 -0700

    cosmetic

--- Scripts/Compass.cs
+++ Scripts/Compass.cs
@@ -2,8 +2,6 @@
 
 namespace Shiv.Scripts {
 	class Compass : Script {
-		public override void OnTick() {
-			UI.DrawText(.49f, .01f, $"{AbsHeading(Heading(CameraMatrix)):F0}");
-		}
+		public override void OnTick() => UI.DrawText(.49f, .01f, $"{AbsHeading(Heading(CameraMatrix)):F0}");
 	}
 }
