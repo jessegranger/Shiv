4b8341b804e26d79f1fa398d065e72b9c0a7138b
Jesse Granger <jesse@conductrics.com>
2019-07-28 11:43:30 -0700

    Add GetModelPlanes() and IntersectModel(ray)

--- Game/World.cs
+++ Game/World.cs
@@ -5,8 +5,15 @@ using GTA.Native;
 using System.Numerics;
 using static GTA.Native.Function;
 using static GTA.Native.Hash;
+using static System.Math;
+using System.Collections.Generic;
 
 namespace Shiv {
+	public struct FinitePlane {
+		public Vector3 Center;
+		public Vector3 Normal;
+		public Vector3 Size;
+	}
 	public static partial class Global {
 
 		public static PointF ScreenCoords(Vector3 pos) {
@@ -54,79 +61,84 @@ namespace Shiv {
 			DrawLine(tBack, Vector3.Transform(backLeft + d, m), Color.Pink);
 		}
 
-		public static void GetNormals(Matrix4x4 m, Vector3 backLeft, Vector3 frontRight, out Vector3[] corners, out Vector3[] centers, out Vector3[] normals) {
-			corners = new Vector3[6];
-			centers = new Vector3[6];
-			normals = new Vector3[6];
+
+		public static IEnumerable<FinitePlane> GetModelPlanes(Matrix4x4 m, Vector3 backLeft, Vector3 frontRight) {
+			var corners = new Vector3[6];
+			var centers = new Vector3[6];
+			var normals = new Vector3[6];
+			var sizes = new Vector3[6];
 			var w = West * (frontRight.X - backLeft.X);
 			var d = North * (frontRight.Y - backLeft.Y);
 			var h = Up * (frontRight.Z - backLeft.Z);
 			var tFront = Vector3.Transform(frontRight, m);
-			var tBack = Vector3.Transform(backLeft, m);
+			// var tBack = Vector3.Transform(backLeft, m);
 			corners[0] = Vector3.Transform(frontRight + w, m);
 			corners[1] = Vector3.Transform(frontRight - h, m);
-			corners[2] = Vector3.Transform(frontRight - d, m);
-			corners[3] = Vector3.Transform(backLeft - w, m);
-			corners[4] = Vector3.Transform(backLeft + h, m);
-			corners[5] = Vector3.Transform(backLeft + d, m);
 
 			centers[0] = (corners[0] + corners[1]) * .5f;
-			centers[1] = (corners[0] + corners[2]) * .5f;
-			centers[2] = (corners[1] + corners[2]) * .5f;
-			centers[3] = (corners[3] + corners[4]) * .5f;
-			centers[4] = (corners[3] + corners[5]) * .5f;
-			centers[5] = (corners[4] + corners[5]) * .5f;
-
 			normals[0] = Vector3.Normalize(Vector3.Cross(corners[1] - tFront, corners[0] - tFront));
+			sizes[0] = new Vector3(Abs(corners[0].X - centers[0].X), Abs(corners[0].Y - centers[0].Y), Abs(corners[0].Z - centers[0].Z));
+			yield return new FinitePlane() { Center = centers[0], Normal = normals[0], Size = sizes[0] };
+
+			corners[2] = Vector3.Transform(frontRight - d, m);
+			centers[1] = (corners[0] + corners[2]) * .5f;
 			normals[1] = Vector3.Normalize(Vector3.Cross(corners[0] - tFront, corners[2] - tFront));
+			sizes[1] = new Vector3(Abs(corners[1].X - centers[1].X), Abs(corners[1].Y - centers[1].Y), Abs(corners[1].Z - centers[1].Z));
+			yield return new FinitePlane() { Center = centers[1], Normal = normals[1], Size = sizes[1] };
+
+			centers[2] = (corners[1] + corners[2]) * .5f;
 			normals[2] = Vector3.Normalize(Vector3.Cross(corners[2] - tFront, corners[1] - tFront));
+			sizes[2] = new Vector3(Abs(corners[2].X - centers[2].X), Abs(corners[2].Y - centers[2].Y), Abs(corners[2].Z - centers[2].Z));
+			yield return new FinitePlane() { Center = centers[2], Normal = normals[2], Size = sizes[2] };
+			
+			corners[3] = Vector3.Transform(backLeft - w, m);
+			corners[4] = Vector3.Transform(backLeft + h, m);
+			centers[3] = (corners[3] + corners[4]) * .5f;
 			normals[3] = -normals[0];
+			sizes[3] = new Vector3(Abs(corners[3].X - centers[3].X), Abs(corners[3].Y - centers[3].Y), Abs(corners[3].Z - centers[3].Z));
+			yield return new FinitePlane() { Center = centers[3], Normal = normals[3], Size = sizes[3] };
+
+			corners[5] = Vector3.Transform(backLeft + d, m);
+			centers[4] = (corners[3] + corners[5]) * .5f;
 			normals[4] = -normals[1];
+			sizes[4] = new Vector3(Abs(corners[4].X - centers[4].X), Abs(corners[4].Y - centers[4].Y), Abs(corners[4].Z - centers[4].Z));
+			yield return new FinitePlane() { Center = centers[4], Normal = normals[4], Size = sizes[4] };
+
+			centers[5] = (corners[4] + corners[5]) * .5f;
 			normals[5] = -normals[2];
+			sizes[5] = new Vector3(Abs(corners[5].X - centers[5].X), Abs(corners[5].Y - centers[5].Y), Abs(corners[5].Z - centers[5].Z));
+			yield return new FinitePlane() { Center = centers[5], Normal = normals[5], Size = sizes[5] };
 
 		}
 
 		public static Vector3 GetCenter(Matrix4x4 m, Vector3 backLeft, Vector3 frontRight) => Vector3.Transform((backLeft + frontRight) / 2f, m);
 
-		public static bool TryIntersectPlane(Vector3 rayStart, Vector3 rayEnd, Vector3 corner, Vector3 normal, Vector3 size, out Vector3 point) {
+		public static bool TryIntersectPlane(Vector3 rayStart, Vector3 rayDir, FinitePlane plane, out Vector3 point) {
 			point = Vector3.Zero;
-			Vector3 rayDir = rayEnd - rayStart;
 
-			float denom = Vector3.Dot(normal, rayDir);
+			float denom = Vector3.Dot(plane.Normal, rayDir);
 			if( denom == 0 ) {
 				return false;
 			}
-			float r = Vector3.Dot(normal, (corner - rayStart)) / denom;
+			float r = Vector3.Dot(plane.Normal, (plane.Center - rayStart)) / denom;
 			if( r >= 0f && r <= 1f ) {
 				point = rayStart + (r * rayDir);
 				// check if point is inside the rect
-				UI.DrawTextInWorld(corner, $"r:{r}");
-				return IsBetween(corner.X - size.X, corner.X + size.X, point.X)
-					&& IsBetween(corner.Y - size.Y, corner.Y + size.Y, point.Y)
-					&& IsBetween(corner.Z - size.Z, corner.Z + size.Z, point.Z);
+				return 
+					IsBetween(plane.Center.X - plane.Size.X, plane.Center.X + plane.Size.X, point.X) &&
+					IsBetween(plane.Center.Y - plane.Size.Y, plane.Center.Y + plane.Size.Y, point.Y) &&
+					IsBetween(plane.Center.Z - plane.Size.Z, plane.Center.Z + plane.Size.Z, point.Z);
 			}
 			return false;
 		}
 
 		public static bool IntersectModel(Vector3 rayStart, Vector3 rayEnd, Matrix4x4 m, Vector3 backLeft, Vector3 frontRight) {
-			Vector3 rayDir = (rayEnd - rayStart);
-			GetNormals(m, backLeft, frontRight, out var corners, out var centers, out var normals);
-			/*
-			foreach( var n in normals ) {
-				if( Vector3.Dot(rayDir, n) == 0f ) {
-					return false;
-				}
-			}
-			*/
-
-			for( int i = 0; i < 6; i++) {
-				var s = Vector3.Dot(normals[i], centers[i] - rayStart) / Vector3.Dot(normals[i], rayDir);
-				UI.DrawTextInWorld(centers[i], $"s:{s:F2}");
-				if( s > 0f && s < 1f ) {
+			Vector3 rayDir = rayEnd - rayStart;
+			foreach(var plane in GetModelPlanes(m, backLeft, frontRight) ) {
+				if( TryIntersectPlane(rayStart, rayDir, plane, out Vector3 _) ) {
 					return true;
 				}
 			}
-
 			return false;
 		}
 
